22c9ac34427adec20ee74fbb49c5bc306cf05370
Ahlam <Ahlam.musemohamed@gmail.com>
2025-01-22 20:40:48 -0600

    created a header

---
+++ app/(tabs)/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Header from '../../components/Header'
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Header />
+    </View>
+  );
+}
---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Image from "../assets/images/ellipse2.jpg"
+import { Appbar, Avatar } from "react-native-paper";
+
+const Header = () => {
+  return (
+    <Appbar.Header style={styles.header}>
+      <Avatar.Image
+        size={40}
+        source={require("../assets/images/ellipse2.jpg")}
+        style={styles.avatar}
+      />
+      <Appbar.Action icon="bell-badge-outline" size={29} color="#573926" onPress={() => {}} />
+    </Appbar.Header>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#FBFBFB", // Customize the header color
+    justifyContent: "space-between",
+    paddingRight: 25,
+    paddingLeft: 25,
+  },
+  avatar: {
+    marginRight: 10, // Add some margin to position the avatar nicely
+  },
+});
+
+export default Header;
