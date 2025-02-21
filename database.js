30b8465072a3e6a2e689a08dadeb4cd28da99463
Ahlam Mohamed <157087403+Mohamed-Ahlam@users.noreply.github.com>
2025-02-20 19:06:39 -0600

    Update database.js

--- database.js
+++ database.js
@@ -6,12 +6,3 @@ module.exports = mysql.createConnection({
     password: 'foodie',
     database: 'Users'
 })
-
-/* 
-this createConnection method is used to create a connection then export
-
-then this connection will be inported to where we need to use it (inside our routes folder so our routes like GET can be connected to the database)
-
-host = localhost bc we are logged into our local mysql server
-
-*/
